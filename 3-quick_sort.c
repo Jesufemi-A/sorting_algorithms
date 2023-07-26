4fb35f7fb6dcbf0aecfa184f4517392ebdb42c2d
Jesufemi-A <jesufemia1@gmail.com>
2023-07-25 20:17:54 -0700

    implementing lomuto partition scheme to sort unordered array and implementing quicksort algorithm

---
+++ 3-quick_sort.c
@@ -0,0 +1,95 @@
+#include "sort.h"
+
+void swap_ints(int *a, int *b);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
+void quick_sort(int *array, size_t size);
+
+/**
+ * swap_ints - substituting two int values in unsorted array.
+ * @a: first int value to be sorted.
+ * @b: second int value to be sorted.
+ */
+
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * lomuto_partition - using lomuto partition scheme to order a 
+ * subset of int array
+ * @array: int array
+ * @size: length or size of the array.
+ * @left: index start point of unordered subset
+ * @right: index end point of unordered subset
+ * Return: final index partition
+ */
+
+int lomuto_partition(int *array, size_t size, int left, int right)
+{
+	int *pivot, above, below;
+
+	pivot = array + right;
+	for (above = below = left; below < right; below++)
+	{
+		if (array[below] < *pivot)
+		{
+			if (above < below)
+			{
+				swap_ints(array + below, array + above);
+				print_array(array, size);
+			}
+			above++;
+		}
+	}
+
+	if (array[above] > *pivot)
+	{
+		swap_ints(array + above, pivot);
+		print_array(array, size);
+	}
+
+	return (above);
+}
+
+/**
+ * lomuto_sort - using recursion to activate the quicksort algorithm
+ * @array: unsorted int array
+ * @size: length or sizeof array
+ * @left: index start point of unordered array
+ * @right: index end point of unordered array
+ * Description: implementing the Lomuto partition scheme.
+ */
+
+void lomuto_sort(int *array, size_t size, int left, int right)
+{
+	int part;
+
+	if (right - left > 0)
+	{
+		part = lomuto_partition(array, size, left, right);
+		lomuto_sort(array, size, left, part - 1);
+		lomuto_sort(array, size, part + 1, right);
+	}
+}
+
+/**
+ * quick_sort - sorting unordered array via
+ * quicksort algorithm
+ * @array: int array
+ * @size: length or size of the array.
+ * Description: implementing lomuto partition sheme
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	lomuto_sort(array, size, 0, size - 1);
+}
