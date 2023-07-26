1b73eb227032ea966c8ec7497a2a1532cab7b7bc
Jesufemi-A <jesufemia1@gmail.com>
2023-07-25 19:22:08 -0700

    implementing sorting algorithms

---
+++ 0-bubble_sort.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+
+/**
+ * swap_ints - Substituting 2 num values (int) in array
+ * @a: argument 1 to be substituted
+ * @b: argument 2 to be subsstituted
+ */
+
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * bubble_sort - Sorting int array in increasing(ascending)
+ * order
+ * @array: parameter for intended unsorted array
+ * @size: length or size of the array
+ * Description: Print  array to stdout after iteration
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, len = size;
+	bool bubbly = false;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (i = 0; i < len - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap_ints(array + i, array + i + 1);
+				print_array(array, size);
+				bubbly = false;
+			}
+		}
+		len--;
+	}
+}
